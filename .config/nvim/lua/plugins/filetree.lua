b0a2410e55e9eed8c37a57d5af3e625a15b14512
Aman Das <amandas62640@gmail.com>
2024-02-15 22:25:41 +0530

    better neotree and keymap terminal

---
+++ .config/nvim/lua/plugins/filetree.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          -- always_show = {
+          --   ".config",
+          --   ".gitignore*",
+          --   ".zshrc"
+          -- }
+        }
+      }
+    }
+  }
+}
