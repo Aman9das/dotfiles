ecbb6c3da6ddb6b7f1b63f7d6bb6960807bac547
Aman Das <amandas62640@gmail.com>
2024-02-16 00:54:03 +0530

    no unnecessary colors

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,23 @@
 return {
   -- add base16 colorschemes
-  {
-    "RRethy/nvim-base16",
-    -- priority = 500,
-  },
+  -- {
+  --   "RRethy/nvim-base16",
+  --   -- priority = 500,
+  -- },
 
   -- tairiki scheme
-  {
-    "deparr/tairiki.nvim",
-    -- lazy = false,
-    -- priority = 1000,
-  },
+  -- {
+  --   "deparr/tairiki.nvim",
+  --   -- lazy = false,
+  --   -- priority = 1000,
+  -- },
 
   -- github themes
-  {
-    'projekt0n/github-nvim-theme',
-    -- lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-    -- priority = 1000, -- make sure to load this before all the other start plugins
-  },
+  -- {
+  --   'projekt0n/github-nvim-theme',
+  --   -- lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+  --   -- priority = 1000, -- make sure to load this before all the other start plugins
+  -- },
 
   {
     "HoNamDuong/hybrid.nvim",
