63273efd4ee0917b2065c4e7f30752a9b43be0d6
Aman Das <amandas62640@gmail.com>
2024-02-16 19:18:30 +0530

    neovim-project to handle projects

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -1,14 +1,40 @@
 return {
   {
-    "nvim-telescope/telescope-project.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" },
-    event = "VeryLazy",
-    keys = {
-      {
-        "<leader>fp",
-        function() require("telescope").extensions.project.project {} end,
-        desc = "Projects",
+    "coffebar/neovim-project",
+    opts = {
+      projects = { -- define project roots
+        "~/Projects/*",
+        "~/dotfiles/",
       },
+      last_session_on_startup = false,
     },
+    init = function()
+      -- enable saving the state of plugins in the session
+      vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+    end,
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+      { "Shatur/neovim-session-manager" },
+    },
+    lazy = false,
+    priority = 100,
+  },
+  {
+    "nvimdev/dashboard-nvim",
+    optional = true,
+    opts = function(_, opts)
+      local projects = {
+        action = "Telescope neovim-project history",
+        desc = " Projects",
+        icon = " ",
+        key = "p",
+      }
+
+      projects.desc = projects.desc .. string.rep(" ", 43 - #projects.desc)
+      projects.key_format = "  %s"
+
+      table.insert(opts.config.center, 3, projects)
+    end,
   }
 }
