3a33a368492fdfb21eb42f5b3297a0e8544cbd35
Aman Das <amandas62640@gmail.com>
2024-02-19 16:41:40 +0530

    .nvim.lua not stowed, search grep works too

--- .nvim.lua
+++ .nvim.lua
@@ -1,1 +1,11 @@
 require("neo-tree").config.filesystem.filtered_items.hide_dotfiles = false -- neo-tree show dotfiles
+vim.keymap.set(
+	"n",
+	"<leader>sg",
+	'<cmd>lua require("telescope.builtin").grep_string({ additional_args = { "--hidden" } })<cr>'
+)
+vim.keymap.set(
+	"n",
+	"<leader>sG",
+	'<cmd>lua require("telescope.builtin").grep_string({ cwd = vim.fn.expand "%:p:h", additional_args = { "--hidden" } })<cr>'
+)
