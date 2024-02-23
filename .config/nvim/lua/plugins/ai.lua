9adece5ef5c44b89145c2fc4a56fdd81db436139
Aman Das <39594914+Aman9das@users.noreply.github.com>
2024-02-24 04:00:35 +0530

    add chatgpt and zsh

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -14,4 +14,23 @@ return {
       theme_file = "/var/home/aman/.cache/oatmeal/base16-tomorrow-night.tmTheme",
     },
   },
+
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup({
+        api_key_cmd = "bw get notes chatgpt-nvim-secret-key --nointeraction --session j1h551YdVAIQRWmzey+EhEFY4t7JLRNd3co1cWc0X8ldnl4pkO3gY7oGCe1+XBvWwtZh6f57ycNS+ZnRDZabrg==",
+        popup_layout = {
+          default = "right",
+        },
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "folke/trouble.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
 }
--- .zshrc
+++ .zshrc
@@ -66,12 +66,13 @@ export GPG_TTY=$TTY
 
 # Source additional local files if they exist.
 z4h source ~/.env.zsh
+z4h source ~/.config/wezterm/zsh.sh
 
 # Use additional Git repositories pulled in with `z4h install`.
 #
 # This is just an example that you should delete. It does nothing useful.
-z4h source ohmyzsh/ohmyzsh/lib/diagnostics.zsh  # source an individual file
-z4h load   ohmyzsh/ohmyzsh/plugins/emoji-clock  # load a plugin
+# z4h source ohmyzsh/ohmyzsh/lib/diagnostics.zsh  # source an individual file
+# z4h load   ohmyzsh/ohmyzsh/plugins/emoji-clock  # load a plugin
 
 # Define key bindings.
 z4h bindkey z4h-backward-kill-word  Ctrl+Backspace     Ctrl+H
