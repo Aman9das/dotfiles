ecaa97bce8945c2906429b815d622d6163ae18ef
Aman Das <39594914+Aman9das@users.noreply.github.com>
2024-02-23 11:14:04 +0530

    explicit leader for dotfyle

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,8 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.env.SHELL = '/usr/bin/zsh'
-vim.env.PATH = '~/.local/bin/:' .. vim.env.PATH
+vim.env.PATH = "~/.local/bin/:" .. vim.env.PATH
+vim.g.mapleader = " "
+
 vim.opt.wildignorecase = true
 vim.opt.exrc = true
 vim.opt.conceallevel = 0 -- No hiding special characters
