3951f535e940a6de375c29688fa218a463df2e69
Aman Das <39594914+Aman9das@users.noreply.github.com>
2024-02-23 15:49:34 +0530

    improved ai

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -5,12 +5,12 @@ return {
     cmd = { "Oatmeal" },
     keys = {
       { "<leader>o", desc = "other" },
-      { "<leader>om", mode = "n", desc = "Start Oatmeal session" },
-      { "<leader>om", mode = "v", desc = "Start Oatmeal session" },
+      { "<leader>om", mode = "n", desc = "Oatmeal session" },
+      { "<leader>om", mode = "v", desc = "Oatmeal session on chunk" },
     },
     opts = {
       backend = "ollama",
-      model = "deepseek-coder:6.7b",
+      model = "codellama:7b",
       theme_file = "/var/home/aman/.cache/oatmeal/base16-tomorrow-night.tmTheme",
     },
   },
