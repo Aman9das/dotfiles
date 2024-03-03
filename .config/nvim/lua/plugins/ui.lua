bec667005052281a5112077aea833efe545e79ce
Aman Das <39594914+Aman9das@users.noreply.github.com>
2024-03-03 15:10:29 +0530

    better neo-tree navigation

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -8,4 +8,30 @@ return {
       },
     },
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        mappings = {
+          ["Y"] = {
+            function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              vim.fn.setreg("+", path, "c")
+            end,
+            desc = "copy path to clipboard",
+          },
+          ["="] = {
+            function(state)
+              local path = state.path -- this gives you the path
+
+              -- do whatever you want to do here
+              vim.fn.chdir(path)
+            end,
+            desc = "cd to current root",
+          },
+        },
+      },
+    },
+  },
 }
