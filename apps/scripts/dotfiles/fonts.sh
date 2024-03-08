9d10eace9c808dd2f8715b180b48f4d2a84b01e1
Aman Das <39594914+Aman9das@users.noreply.github.com>
2024-03-08 19:14:32 +0530

    fix font url

--- apps/scripts/dotfiles/fonts.sh
+++ apps/scripts/dotfiles/fonts.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Download the Clear Sans font archive
-wget https://api.fontsource.org/v1/download/clear-sans -O clear-sans.zip
-unzip -j clear-sans.zip 'ttf/*' -d ~/.local/share/fonts/clear-sans
+wget https://www.1001fonts.com/download/clear-sans.zip -O clear-sans.zip
+unzip -j clear-sans.zip 'TTF/*' -d ~/.local/share/fonts/clear-sans
 rm clear-sans.zip
 
 # Download the Intel One Mono Nerd Font font archive
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/IntelOneMono.zip -O intel-one-mono.zip
-unzip intel-one-mono.zip "IntoneMonoNerdFont-*" -d ~/.local/share/fonts/intel-one-mono
+unzip -j intel-one-mono.zip "IntoneMonoNerdFont-*" -d ~/.local/share/fonts/intel-one-mono
 rm intel-one-mono.zip
