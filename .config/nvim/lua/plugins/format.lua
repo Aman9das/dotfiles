23b093bce87cfe5110787563f42f47f57143cbd7
Aman Das <39594914+Aman9das@users.noreply.github.com>
2024-03-15 16:21:04 +0530

    fix injected langs AHHHHH

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -1,6 +1,16 @@
 return {
   {
     "stevearc/conform.nvim",
+    keys = {
+      {
+        "<leader>cF",
+        function()
+          require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+        end,
+        mode = { "n", "v" },
+        desc = "Format Injected Langs",
+      },
+    },
     opts = {
       formatters_by_ft = {
         -- r formatter
