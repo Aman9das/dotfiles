084c69d037ddb6c197abda4f4569b1c310fd745b
Aman Das <39594914+Aman9das@users.noreply.github.com>
2024-03-29 11:12:55 +0530

    repl hopefully works well

--- .config/nvim/lua/plugins/quarto.lua
+++ .config/nvim/lua/plugins/quarto.lua
@@ -40,28 +40,6 @@ return {
     dependencies = {
       "jmbuhr/otter.nvim",
     },
-    keys = {
-      { "<leader><cr>", ":QuartoSend<cr>", desc = "send code chunk" },
-      { "<c-cr>", ":QuartoSend<cr>", desc = "send code chunk" },
-      {
-        "<c-cr>",
-        "<esc>:QuartoSend<cr>a",
-        mode = "i",
-        desc = "send code chunk",
-      },
-      {
-        "<c-cr>",
-        "<Plug>SlimeSend<cr>",
-        mode = "v",
-        desc = "send code chunk",
-      },
-      {
-        "<cr>",
-        "<Plug>SlimeSend<cr>",
-        mode = "v",
-        desc = "send code chunk",
-      },
-    },
   },
 
   {
@@ -88,57 +66,6 @@ return {
   --     opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "otter" } }))
   --   end,
   -- },
-
-  -- send code from python/r/qmd documets to a terminal or REPL
-  -- like ipython, R, bash
-  {
-    "jpalardy/vim-slime",
-    init = function()
-      vim.b["quarto_is_python_chunk"] = false
-      Quarto_is_in_python_chunk = function()
-        require("otter.tools.functions").is_otter_language_context("python")
-      end
-      vim.b["quarto_is_" .. "r" .. "_chunk"] = false
-      Quarto_is_in_r_chunk = function()
-        require("otter.tools.functions").is_otter_language_context("r")
-      end
-
-      vim.cmd([[
-      let g:slime_dispatch_ipython_pause = 100
-      function SlimeOverride_EscapeText_quarto(text)
-      call v:lua.Quarto_is_in_python_chunk()
-      call v:lua.Quarto_is_in_r_chunk()
-
-      if exists('g:slime_python_ipython') && len(split(a:text,"\n")) > 1 && b:quarto_is_python_chunk && !(exists('b:quarto_is_r_mode') && b:quarto_is_r_mode)
-      return ["%cpaste -q\n", g:slime_dispatch_ipython_pause, a:text, "--", "\n"]
-      else
-      return [a:text]
-      end
-
-      endfunction
-      ]])
-
-      vim.g.slime_target = "neovim"
-      vim.g.slime_python_ipython = 1
-    end,
-    config = function()
-      local function mark_terminal()
-        vim.g.slime_last_channel = vim.b.terminal_job_id
-        print("terminal job id: " .. vim.b.terminal_job_id)
-      end
-
-      local function set_terminal()
-        vim.b.slime_config = { jobid = vim.g.slime_last_channel }
-      end
-      vim.keymap.set(
-        "n",
-        "<leader>cM",
-        mark_terminal,
-        { desc = "mark terminal" }
-      )
-      vim.keymap.set("n", "<leader>cs", set_terminal, { desc = "set terminal" })
-    end,
-  },
 
   -- paste an image from the clipboard or drag-and-drop
   {
