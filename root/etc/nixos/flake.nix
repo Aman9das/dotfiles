21c162c5bc4890a88c2743b4b8c44a122e83599c
Aman Das <39594914+Aman9das@users.noreply.github.com>
2024-04-16 04:25:53 +0530

    rm mirror

--- root/etc/nixos/flake.nix
+++ root/etc/nixos/flake.nix
@@ -6,7 +6,6 @@
     experimental-features = [ "nix-command" "flakes" ];
     substituters = [
       # Replace the official cache with a mirror located in China
-      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://cache.nixos.org/"
     ];
     extra-substituters = [
@@ -26,10 +25,10 @@
     # which represents the GitHub repository URL + branch/commit-id/tag.
 
     # Official NixOS package source, using nixos-23.05 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     # home-manager, used for managing user configuration
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with the `inputs.nixpkgs` of the current flake,
       # to avoid problems caused by different versions of nixpkgs.
@@ -90,7 +89,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
 
-            # TODO replace ryan with your own username
+            # DONE replace ryan with your own username
             home-manager.users.aman = import ./home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
